cb87b300d5c41951bcd2c03a8f9bb7ee342c30b0
Delfio Francisco <delfio_eu@hotmail.com>
2022-09-20 16:11:32 -0400

    feat: adicionando palheta de cores padrão

---
+++ chakra-config.ts
@@ -0,0 +1,32 @@
+import {
+    extendTheme
+} from "@chakra-ui/react";
+
+
+const theme = extendTheme({
+    colors: {
+        "_orange": {
+            100: "#FF6736",
+            200: "#FC6433",
+            300: "#F55B29",
+            400: "#EC4E1C",
+            500: "#E34715",
+            600: "#E33A15",
+            700: "#DD3E1C",
+            800: "#D63F1E",
+            900: "#F35724"
+        },
+        "_blue": "#009ABC",
+        "_green": "#00CF9A",
+        "_purple": "#874A9F",
+        "_red": "#993950",
+        "_default_bg": "#F5FCFF"
+    },
+    fonts: {
+        body: "Inter, Roboto, sans-serif",
+        heading: "Inter, Roboto, sans-serif",
+        mono: "Roboto, sans-serif",
+    }
+})
+
+export default theme;
--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import IReactComponentType from "../types/IReactComponentType";
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../../chakra-config";
 
 const AppContext: IReactComponentType = ({ children }) => {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 };
 
 export default AppContext;
