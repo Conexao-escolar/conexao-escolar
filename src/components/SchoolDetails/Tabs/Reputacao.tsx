c509d55cef81f0c830a4c77060ed5e8bb88f5163
Delfio Francisco <delfio_eu@hotmail.com>
2022-10-03 10:09:38 -0400

    feat: cards de reputação por tags

--- src/components/SchoolDetails/Tabs/Reputacao.tsx
+++ src/components/SchoolDetails/Tabs/Reputacao.tsx
@@ -3,122 +3,113 @@ import React from "react";
 import { MdStar } from "react-icons/md";
 import { IReputacao } from "../../../types/IEscola";
 // import { Container } from './styles';
+import formatRank from "../../../utils/formatRank";
 
 type IProps = {
-    reputacao: IReputacao
-}
+  reputacao: IReputacao;
+};
 
-const Reputacao: React.FC<IProps> = ({reputacao}) => {
+const Reputacao: React.FC<IProps> = ({ reputacao = {} }) => {
+  const AllKeys = React.useMemo(() => Object.keys(reputacao), [reputacao]);
+
+  const rankFormated = React.useCallback(
+    (rank: number) => formatRank(rank),
+    []
+  );
+
+  const Reputacao = React.useCallback(
+    (
+      reputacao: "Péssimo" | "Ruim" | "Bom" | "Muito bom" | "Excelente",
+      rank: number,
+      key: string
+    ) => {
+      if (reputacao === "Ruim" || reputacao === "Péssimo")
+        return (
+          <>
+            <Text>
+              Reputação em
+              <span className="tag"> #{key}</span>
+            </Text>
+            <Text fontSize="3xl" fontWeight="bold" textColor="#874A9F">
+              {reputacao}
+            </Text>
+            <Flex flexDir="row" justifyContent="space-around" w="full">
+              <Flex>
+                <Icon as={MdStar} boxSize={5} color="#874A9F" />
+                <Text>{rank}</Text>
+              </Flex>
+              <Text>Ver reputação detalhada</Text>
+            </Flex>
+          </>
+        );
+
+      if (reputacao === "Excelente") {
+        return (
+          <>
+            <Text>
+              Reputação em
+              <span className="tag"> #{key}</span>
+            </Text>
+            <Text fontSize="3xl" fontWeight="bold" textColor="#FF6736">
+              {reputacao}
+            </Text>
+            <Flex flexDir="row" justifyContent="space-around" w="full">
+              <Flex>
+                <Icon as={MdStar} boxSize={5} color="#FF6736" />
+                <Text>{rank}</Text>
+              </Flex>
+              <Text>Ver reputação detalhada</Text>
+            </Flex>
+          </>
+        );
+      }
+
+      return (
+        <>
+          <Text>
+            Reputação em
+            <span className="tag"> #{key}</span>
+          </Text>
+          <Text fontSize="3xl" fontWeight="bold" textColor="#00CF9A">
+            {reputacao}
+          </Text>
+          <Flex flexDir="row" justifyContent="space-around" w="full">
+            <Flex>
+              <Icon as={MdStar} boxSize={5} color="#00CF9A" />
+              <Text>{rank}</Text>
+            </Flex>
+            <Text>Ver reputação detalhada</Text>
+          </Flex>
+        </>
+      );
+    },
+    []
+  );
+
   return (
     <Grid templateColumns="repeat(3, 1fr)" gap={6}>
-      <GridItem>
-        <Box p={5}>
-          <Flex
-            flexDir="column"
-            alignItems="center"
-            justifyContent="space-around"
-            border="1px solid #6C6C6C"
-            borderRadius="5px"
-            minH="150px"
-          >
-            <Text>
-              Reputação em
-              <span className="tag"> #Autismo</span>
-            </Text>
-            <Text fontSize="3xl" fontWeight="bold" textColor="#FF6736">
-              Excelente
-            </Text>
-            <Flex flexDir="row" justifyContent="space-around" w="full">
-              <Flex>
-                <Icon as={MdStar} boxSize={5} color="#FF6736" />
-                <Text>4,9</Text>
+      {AllKeys.map((key) => {
+        const { rank = 0 } = reputacao[key];
+
+        const thisRank = rankFormated(rank);
+
+        return (
+          <GridItem key={`tab-reputacao-${key}`}>
+            <Box p={5}>
+              <Flex
+                flexDir="column"
+                alignItems="center"
+                justifyContent="space-around"
+                border="1px solid #6C6C6C"
+                borderRadius="5px"
+                minH="150px"
+              >
+                {Reputacao(thisRank, rank, key)}
               </Flex>
-              <Text>Ver reputação detalhada</Text>
-            </Flex>
-          </Flex>
-        </Box>
-      </GridItem>
-      <GridItem>
-        <Box p={5}>
-          <Flex
-            flexDir="column"
-            alignItems="center"
-            justifyContent="space-around"
-            border="1px solid #6C6C6C"
-            borderRadius="5px"
-            minH="150px"
-          >
-            <Text>
-              Reputação em
-              <span className="tag"> #Autismo</span>
-            </Text>
-            <Text fontSize="3xl" fontWeight="bold" textColor="#FF6736">
-              Excelente
-            </Text>
-            <Flex flexDir="row" justifyContent="space-around" w="full">
-              <Flex>
-                <Icon as={MdStar} boxSize={5} color="#FF6736" />
-                <Text>4,9</Text>
-              </Flex>
-              <Text>Ver reputação detalhada</Text>
-            </Flex>
-          </Flex>
-        </Box>
-      </GridItem>
-      <GridItem>
-        <Box p={5}>
-          <Flex
-            flexDir="column"
-            alignItems="center"
-            justifyContent="space-around"
-            border="1px solid #6C6C6C"
-            borderRadius="5px"
-            minH="150px"
-          >
-            <Text>
-              Reputação em
-              <span className="tag"> #Autismo</span>
-            </Text>
-            <Text fontSize="3xl" fontWeight="bold" textColor="#FF6736">
-              Excelente
-            </Text>
-            <Flex flexDir="row" justifyContent="space-around" w="full">
-              <Flex>
-                <Icon as={MdStar} boxSize={5} color="#FF6736" />
-                <Text>4,9</Text>
-              </Flex>
-              <Text>Ver reputação detalhada</Text>
-            </Flex>
-          </Flex>
-        </Box>
-      </GridItem>
-      <GridItem>
-        <Box p={5}>
-          <Flex
-            flexDir="column"
-            alignItems="center"
-            justifyContent="space-around"
-            border="1px solid #6C6C6C"
-            borderRadius="5px"
-            minH="150px"
-          >
-            <Text>
-              Reputação em
-              <span className="tag"> #Autismo</span>
-            </Text>
-            <Text fontSize="3xl" fontWeight="bold" textColor="#FF6736">
-              Excelente
-            </Text>
-            <Flex flexDir="row" justifyContent="space-around" w="full">
-              <Flex>
-                <Icon as={MdStar} boxSize={5} color="#FF6736" />
-                <Text>4,9</Text>
-              </Flex>
-              <Text>Ver reputação detalhada</Text>
-            </Flex>
-          </Flex>
-        </Box>
-      </GridItem>
+            </Box>
+          </GridItem>
+        );
+      })}
     </Grid>
   );
 };
