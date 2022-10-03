61e7be61c6971d13ee5e8bc8f80421d3fbe32797
Delfio Francisco <delfio_eu@hotmail.com>
2022-10-03 06:55:47 -0400

    fix: melhoria ao abrir os detalhes das escolas

--- src/components/CardEscola/SchoolLine.tsx
+++ src/components/CardEscola/SchoolLine.tsx
@@ -2,20 +2,20 @@ import { Box, Flex, Icon, Link, Text } from "@chakra-ui/react";
 import React from "react";
 import { MdStar } from "react-icons/md";
 import ICardEscla from "../../types/IEscola";
+import NextLink from "next/link";
 
-type Props = Omit<Omit<Omit<Omit<ICardEscla, "tags">, "modal">, "cidade">, "endereco"> & {
+type Props = Omit<
+  Omit<Omit<Omit<ICardEscla, "tags">, "modal">, "cidade">,
+  "endereco"
+> & {
   color: string;
   index: number;
 };
 
-const SchoolLine: React.FC<Props> = ({
-  color,
-  id,
-  nome,
-  rank,
-  index,
-}) => {
-
+const SchoolLine: React.FC<Props> = ({ color, id, nome, rank, index }) => {
+  const formatedLink = React.useMemo(() => {
+    return nome.replaceAll(" ", "")
+  }, [nome])
   return (
     <Flex flex="1" justifyContent="space-around" key={`first-${id}`}>
       <Box w="10%">
@@ -24,9 +24,11 @@ const SchoolLine: React.FC<Props> = ({
         </Text>
       </Box>
       <Box w="60%" textAlign="left" paddingLeft={3}>
-        <Link>
-          <Text noOfLines={1}>{nome}</Text>
-        </Link>
+        <NextLink href={`/school/${formatedLink}`}>
+          <Link>
+            <Text noOfLines={1}>{nome}</Text>
+          </Link>
+        </NextLink>
       </Box>
       <Box w="30%" textColor={color}>
         <Icon as={MdStar} color={color} /> {rank}
