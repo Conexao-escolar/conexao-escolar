f87c253ebdb74b2b331694ec46383665ee8c54a4
Delfio Francisco <delfio_eu@hotmail.com>
2022-10-09 21:16:05 -0400

    feat: implementando skelleton nos detalhes da escola

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { Box, Container as ChakraContainer } from "@chakra-ui/react";
+import {
+  Box,
+  Container as ChakraContainer,
+  SkeletonCircle,
+  SkeletonText,
+} from "@chakra-ui/react";
 import { IProps } from "../../types/IReactComponentType";
 import Nav from "../Nav";
 import MenuItems from "../Nav/MenuOpcoes";
@@ -9,6 +14,7 @@ import Rodape from "../Rodape";
 type IContainerProps = IProps & {
   activeMenu: MenuItems;
   extraContainer?: JSX.Element | JSX.Element[];
+  loading?: boolean;
   containerSize?: "container.xl" | "container.lg" | "container.md";
 };
 
@@ -17,12 +23,22 @@ const Container: React.FC<IContainerProps> = ({
   activeMenu = MenuItems.Home,
   extraContainer,
   containerSize = "container.xl",
+  loading,
 }) => {
   return (
     <Box as="main" h="100vh" bg="_default_bg" overflowX="hidden">
       <Nav MenuAtivo={activeMenu} />
-      {extraContainer}
-      <ChakraContainer maxW={containerSize}>{children}</ChakraContainer>
+      {loading ? (
+        <Box padding="60" bg="white">
+          <SkeletonCircle size="10" />
+          <SkeletonText mt="4" noOfLines={4} spacing="4" />
+        </Box>
+      ) : (
+        <>
+          {extraContainer}
+          <ChakraContainer maxW={containerSize}>{children}</ChakraContainer>
+        </>
+      )}
       <Rodape />
     </Box>
   );
